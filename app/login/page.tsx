98526842e56dc9f3e4bcc82fe975002f1fb39d66
vgutolopes <x.victorlopes@gmail.com>
2024-11-09 17:05:55 -0300

    feat: add login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
 import React from "react";
+import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
 
 function LoginPage() {
   return (
     <div className="grid h-full grid-cols-2">
       {/* Esquerda */}
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
-        <Image src="/logo.svg" width={173} height={39} alt="Finance AI" className="mb-8" />
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Finance AI"
+          className="mb-8"
+        />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground">
+        <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
 
-        <Button>Realizar login</Button>
+        <Button variant="outline">
+          <LogInIcon className="mr-2" /> Realizar login
+        </Button>
       </div>
       {/* Direita */}
       <div className="relative h-full w-full">
