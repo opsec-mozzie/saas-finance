febbf03e43ec67457493263c66cca2e0a283079e
opsec-mozzie <opsec.mozzie@proton.me>
2024-11-09 17:29:09 -0300

    feat: add auth with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-function LoginPage() {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       {/* Esquerda */}
@@ -22,9 +30,11 @@ function LoginPage() {
           facilitando o controle do seu orçamento.
         </p>
 
-        <Button variant="outline">
-          <LogInIcon className="mr-2" /> Realizar login
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" /> Realizar login
+          </Button>
+        </SignInButton>
       </div>
       {/* Direita */}
       <div className="relative h-full w-full">
@@ -37,6 +47,6 @@ function LoginPage() {
       </div>
     </div>
   );
-}
+};
 
 export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect("/login");
+
   return (
-    <div className="flex w-screen items-center justify-center">
-      <h1>Home</h1>
-      <Button>Button</Button>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
     </div>
   );
-}
+};
+
+export default Home;
